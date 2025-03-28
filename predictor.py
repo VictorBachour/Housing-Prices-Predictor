8330c6c317ace7197665f1afe355e0d3512cdee7
vbacho <victorbachour@gmail.com>
2025-03-28 15:54:43 -0700

    .52 has best r2 score so far will still add hypertuning maybe something better but code will make it easier to find and will fix large prices

--- predictor.py
+++ predictor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
@@ -19,7 +20,7 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-model = XGBRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
+model = XGBRegressor(n_estimators=100, learning_rate=0.52, seed=42)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
